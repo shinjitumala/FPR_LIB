88853f0fd171a4aadf4787bcf616ae1a7f44f5d1
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-02-01 17:58:41 +0900

    Added warning assertion.

--- include/FPR/debug/assert.h
+++ include/FPR/debug/assert.h
@@ -42,4 +42,41 @@ inline void asrt(
         asm("INT3");
     }
 }
-} // namespace FPR
+
+/**
+ * Executes 'warning' if the condition is not met.
+ * @param condition Condition to execute 'warning'.
+ * @param warning Fuction to be ececuted if 'condition' is false.
+ * @param loc Source location to be displayed if 'condition' is false.
+ */
+inline void warn(
+    bool &&condition,
+    std::function<void()> warning = []() {},
+    std::experimental::source_location loc =
+        std::experimental::source_location::current()) {
+#ifdef DEBUG
+#undef DEBUG
+    static const bool debug{true};
+#else
+    static const bool debug{false};
+#endif
+
+    if constexpr (debug) {
+        if (condition) {
+            return;
+        }
+
+        ANSI_Out{std::cerr} << ANSI_Out::State{
+                                   .bg = ANSI_Out::State::Color::BLACK,
+                                   .fg = ANSI_Out::State::Color::YELLOW,
+                                   .e = ANSI_Out::State::Emphasis::BOLD}
+                            << ">> Warning. Location: " << loc.file_name() << ":" << loc.line() << ":" << loc.column() << " " << loc.function_name() << "\n";
+        warning();
+        ANSI_Out{std::cerr} << ANSI_Out::State{
+                                   .bg = ANSI_Out::State::Color::BLACK,
+                                   .fg = ANSI_Out::State::Color::YELLOW,
+                                   .e = ANSI_Out::State::Emphasis::BOLD}
+                            << "\n<<" << std::endl;
+    }
+}
+} // namespace fpr
