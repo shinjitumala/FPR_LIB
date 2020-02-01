345ff7f7e09dcc3b3a37b75a9d5ef039ffb1bd03
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-02-01 17:57:18 +0900

    Added reset() and is_ansi_streamable.

--- include/FPR/out/ansi_out.h
+++ include/FPR/out/ansi_out.h
@@ -47,11 +47,18 @@ class ANSI_Out {
         enum class Emphasis {
             RESET = 0,
             BOLD = 1,
+            FAINT = 2,
+            ITALIC = 3,
             UNDERLINE = 4,
+            SLOW_BLINK = 5,
+            FAST_BLINK = 6,
             INVERSE = 7,
+            CONCEAL = 8,
+            CROSS_OUT = 9,
             BOLD_OFF = 21,
             UNDERLINE_OFF = 24,
             INVERSE_OFF = 27,
+            FRAMED = 51,
             NONE,
         };
 
@@ -66,9 +73,13 @@ class ANSI_Out {
     /**
      * @param os Output destination.
      */
-    ANSI_Out(std::ostream &os = std::cout);
+    ANSI_Out(std::ostream &os = std::cout) noexcept;
     /** Disallow copy */
     ANSI_Out(const ANSI_Out &) = delete;
+    /**
+     * Destructor. Will reset the color at the end.
+     */
+    ~ANSI_Out();
 
     /**
      * @tparam O Type to be streamed.
@@ -77,7 +88,7 @@ class ANSI_Out {
      * @return ANSI_Out& YES.
      */
     template <class O, class = std::enable_if_t<is_ostreamable<O>::value>>
-    ANSI_Out &operator<<(O o) {
+    ANSI_Out &operator<<(O o) noexcept {
         os << o;
         return *this;
     }
@@ -88,13 +99,33 @@ class ANSI_Out {
      * @param osmanip 
      * @return ANSI_Out& 
      */
-    ANSI_Out &operator<<(osmanip osmanip);
+    ANSI_Out &operator<<(osmanip osmanip) noexcept;
 
     /**
      * Custom overload used for colored terminal output (ANSI)
      * @param state New State
      * @return std::ostream& Destination
      */
-    ANSI_Out &operator<<(const State &state);
+    ANSI_Out &operator<<(const State &state) noexcept;
 };
+
+constexpr inline auto reset = []() -> const auto {
+    ANSI_Out::State s;
+    s.bg = ANSI_Out::State::Color::NONE;
+    s.fg = ANSI_Out::State::Color::NONE;
+    s.e = ANSI_Out::State::Emphasis::NONE;
+    return s;
+};
+
+template <class O>
+struct is_ansi_streamable {
+    template <class T>
+    static auto check(int) -> decltype(std::declval<ANSI_Out &>() << std::declval<T>(), std::true_type());
+    template <class>
+    static auto check(...) -> std::false_type;
+
+  public:
+    static constexpr bool value{std::is_same_v<decltype(check<O>(0)), std::true_type>};
+};
+
 } // namespace fpr
