2464f68a38c884377a489a11a3c16fad70756cdf
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-02-01 17:58:53 +0900

    Added logger.

--- include/FPR/debug/level.h
+++ include/FPR/debug/level.h
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <FPR/out/ansi_out.h>
+#include <ostream>
+
 namespace fpr {
-  protected:
-    enum class Type {
+class Logger : public ANSI_Out {
+  public:
+    /** Log levels */
+    enum class Level {
+        MSG,
+        ERR,
+        WARN,
+        INFO,
     };
-    enum class Level;
+
+    /** Logging level. Any output below this level will be discarded. */
+    static Level output_level;
+    /** Logging level of this message */
+    Level level;
+
+    Logger(Level level) noexcept;
+
+    template <class O, class = std::enable_if_t<is_ansi_streamable<O>::value>>
+    Logger &operator<<(O o) {
+        if (static_cast<uint>(output_level) >= static_cast<uint>(level)) {
+            ANSI_Out::operator<<(o);
+        }
+        return *this;
+    }
+
+  protected:
+    /**
+     * @param level Output level.
+     * @return std::ostream& Approproate ostream for 'level'.
+     */
+    std::ostream &get_ostream(Level level) noexcept;
 };
-}; // namespace FPR
+}; // namespace fpr
--- src/debug/level.cpp
+++ src/debug/level.cpp
@@ -1,10 +1,19 @@
 #include <FPR/debug/level.h>
 
-namespace FPR {
-enum class DebugLevel::Level {
-    INFO,
-    WARNING,
-    ERROR,
-    SILENT
-};
+namespace fpr {
+Logger::Logger(Level level) noexcept : ANSI_Out(get_ostream(level)), level(level) {
 }
+
+Logger::Level Logger::output_level{Level::INFO};
+
+std::ostream &Logger::get_ostream(Level level) noexcept {
+    switch (level) {
+    case Level::MSG:
+    case Level::INFO:
+        return std::cout;
+    case Level::WARN:
+    case Level::ERR:
+        return std::cout;
+    }
+}
+} // namespace fpr
