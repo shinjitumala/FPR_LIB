f767abab796578386adccd79ab9cb1561525598f
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-02-01 17:59:09 +0900

    Fixed escape character being incorrect.

--- src/out/ansi_out.cpp
+++ src/out/ansi_out.cpp
@@ -2,27 +2,38 @@
 #include <ostream>
 
 namespace fpr {
-ANSI_Out::ANSI_Out(std::ostream &os) : os{os} {}
+ANSI_Out::ANSI_Out(std::ostream &os) noexcept : os{os} {}
 
-ANSI_Out &ANSI_Out::operator<<(osmanip o) {
+ANSI_Out &ANSI_Out::operator<<(osmanip o) noexcept {
     os << o;
     return *this;
 }
 
-ANSI_Out &ANSI_Out::operator<<(const State &state) {
-    os << "\033["; // Escape Character
+ANSI_Out &ANSI_Out::operator<<(const State &state) noexcept {
+    os << "\x1b["; // Escape Character
+    bool flag{false};
     if (state.fg != State::Color::NONE) {
         os << static_cast<uint>(state.fg);
+        flag = true;
     }
-    os << ";";
     if (state.bg != State::Color::NONE) {
+        if (flag) {
+            os << ";";
+        }
         os << static_cast<uint>(state.bg) + 10;
+        flag = true;
     }
-    os << ";";
     if (state.e != State::Emphasis::NONE) {
+        if (flag) {
+            os << ";";
+        }
         os << static_cast<uint>(state.e);
     }
     os << "m"; // End
     return *this;
+}
+
+ANSI_Out::~ANSI_Out() {
+    *this << reset();
 }
 } // namespace fpr
