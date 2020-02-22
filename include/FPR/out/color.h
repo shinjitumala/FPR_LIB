72fad0134837e8dcd5bc614a001a9e3798c7298d
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-02-22 22:07:12 +0900

    New convenience class for printing colored terminal output based on ansi color codes.

---
+++ include/FPR/out/color.h
@@ -0,0 +1,68 @@
+/**
+ * @file Colors.h
+ * @author shinjitumala (funny.pig.run@gmail.com)
+ * @version 0.1
+ * @date 2020-02-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ * Provides macros for color printing
+ */
+#pragma once
+
+#include <FPR/out/ansicc.h>
+
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+namespace fpr {
+/** SFINAE to check if a type has operand<< for std::ostream& */
+template <class T>
+class ostreamable {
+    template <class U>
+    static auto test(int) -> decltype(
+        std::declval<std::ostream &>() << std::declval<U>(),
+        std::true_type());
+    template <class>
+    static auto test(...) -> std::false_type;
+
+  public:
+    static constexpr bool v{std::is_same_v<decltype(test<T>(0)), std::true_type>};
+};
+
+/** ansicc out: Conveniece class for outputting colored text */
+class aout {
+    std::ostream &os;
+
+  public:
+    /**
+     * @param cc The color to be set
+     * @param out 
+     */
+    aout(ansicc cc, std::ostream &out = std::cout);
+    ~aout();
+
+    /**
+     * @tparam O 
+     * @tparam std::enable_if_t<ostreamable<O>::v> 
+     * @param o 
+     * @return aout& 
+     */
+    template <class O, class = std::enable_if_t<ostreamable<O>::v>>
+    aout &operator<<(O o) {
+        os << o;
+        return *this;
+    }
+
+    using omanip = std::ostream &(&)(std::ostream &);
+    aout &operator<<(omanip o){
+        os << o;
+        return *this;
+    };
+
+    /** ansicc used to reset color of terminal */
+    static const ansicc reset;
+};
+} // namespace fpr
---
+++ src/out/color.cpp
@@ -0,0 +1,11 @@
+#include <FPR/out/color.h>
+
+namespace fpr {
+aout::aout(ansicc cc, std::ostream &out) : os{out} {
+    os << cc;
+}
+
+aout::~aout() {
+    os << ansicc{};
+}
+} // namespace fpr
