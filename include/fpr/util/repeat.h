d903c88c60930b1196ab7eef8aa4d7ac882b1c53
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-05-15 16:13:51 +0900

    Add safer loop utility.

---
+++ include/fpr/util/repeat.h
@@ -0,0 +1,16 @@
+#include <functional>
+
+namespace fpr {
+using namespace std;
+/// Execute 'action', 'count' times without the danger of accidentally touching
+/// the index unlike for-loops.
+/// @param count
+/// @param action
+void
+repeat(uint count, function<void()> action)
+{
+    for (auto i{ 0 }; i < count; i++) {
+        action();
+    }
+}
+};
